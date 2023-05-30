b98f62465ebb7a365b711e3a0dacf7ca786d9b55
David Quijano III <67296947+dquijano3@users.noreply.github.com>
2023-05-30 10:39:45 -0500

    Update script.js

--- Random Quote Generator/js/script.js
+++ Random Quote Generator/js/script.js
@@ -17,60 +17,71 @@ const quotes = [
   // year: "",
   // tags: ""}
   // 'Quotation Template'
-  {quote: "“Try not. Do or do not. There is no try.”",
+  {quote: "Try not. Do or do not. There is no try.",
    source: "-Yoda, Jedi Master,",
    citation: "Star Wars Episode V: The Empire Strikes Back,",
    year: "", },
-   {quote: "“Inspiration is a guest that does not willingly visit the lazy.”",
+   {quote: "Inspiration is a guest that does not willingly visit the lazy.",
    source: "-Tchaikovsky, Russian Composer,",
    citation: "Pyotr Ilyich Tchaikovsky,",
    year: "1840 - 1893", },
-  {quote: "“Any law which violates the inalienable rights of man is essentially unjust and tyrannical; it is not a law at all.”",
+  {quote: "Any law which violates the inalienable rights of man is essentially unjust and tyrannical; it is not a law at all.",
    source: "-Maximilien Robespierre,",
    citation: "Declaration des droits de l'homme,",
    year: "Aprilr 24, 1793", },
-  {quote: "“Not all those who wander are lost.”",
+  {quote: "Not all those who wander are lost.",
    source: "-Bilbo Baggins,",
    citation: "-The Hobbit,",
    year: " ", },
-  {quote: "“Deeds will not be less valiant because they are unpraised.”",
+  {quote: "Deeds will not be less valiant because they are unpraised.",
    source: "-Aragorn",
    citation: "The Lord of the Rings",
    year: " ", },
-  {quote: "“You step into the road, and if you don't keep your feet, there is no knowing where you might be swept off to.”",
+  {quote: "You step into the road, and if you don't keep your feet, there is no knowing where you might be swept off to.",
   source: "-Bilbo Baggins,",
   citation: "The Lord of the Rings",
   year: " ",},
-  {quote: "“Sometimes we must let go of our pride and do what is requested of us.”",
+  {quote: "Sometimes we must let go of our pride and do what is requested of us.",
   source: "-Anakin Skywalker,",
   citation: "Star Wars Episode II: Attack of The Clones",
   year: "",},
 ]
-console.log(quotes);
+
 
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote () {
   const randomQuote = Math.floor(Math.random() * quotes.length); //just using the length to keep everything adaptable
-  return randomQuote;
+  return quotes [randomQuote];
 };
-console.log(getRandomQuote()); //this was causing me trouble (proper varification of getRandomQuote)
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 /***
  * `printQuote` function
 ***/
 function printQuote() {
-  let quotesObject = quotes[getRandomQuote()];
-  // while counter = () =>
-  quoting =`
-    <p class= "quote">${quotesObject.quote}</p>
-    <p class= "source">${quotesObject.source}</p>
-      <span class= "citation">${quotesObject.citation}</span>
-      <span class= "year">${quotesObject.year}</span>    
-    `
-  document.getElementById('quote-box').innerHTML = quoting; 
+  const quoteElement = document.getElementById("quote");
+  const bodyElement = document.body;
+  
+  const randomQuote = getRandomQuote();
+  const randomColor = generateRandomColor();
+  
+  quoteElement.innerHTML = `
+    <p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}
+    <span class="citation">${randomQuote.citation}</span>
+    <span class="year">${randomQuote.year}</span></p>
+  `;
+  
+  bodyElement.style.backgroundColor = randomColor;
 }
-
 
 /***
  * click event listener for the print quote button
